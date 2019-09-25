d9a1c755bb6516816db82bdc3fe43e531f828e7e
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-09-25 23:23:08 +0300

    eslint fix App.vue

--- bored-app/src/App.vue
+++ bored-app/src/App.vue
@@ -6,13 +6,14 @@
 </template>
 
 <script>
-  import Home from './views/Home';
-  export default {
-    name: 'App',
-    components: {
-      Home,
-    },
-  }
+import Home from './views/Home.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+  },
+};
 </script>
 
 <style lang="scss">
