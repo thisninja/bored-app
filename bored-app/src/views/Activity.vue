269a3bf4c5c6585f2dcbc5fae3b6de01a33a1811
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-09-25 23:11:32 +0300

    init project from vue-cli

---
+++ bored-app/src/views/Activity.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="activity">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Activity',
+  components: {},
+};
+</script>
---
+++ bored-app/src/views/MyList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="my-list">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyList',
+  components: {},
+};
+</script>
